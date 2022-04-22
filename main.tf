edc0c5db1834ba98c4437554a11049b5a37dec6e
Syed Fazli <edfazli92@gmail.com>
2022-04-22 12:33:45 +0800

    adding main.tf backend details

--- main.tf
+++ main.tf
@@ -1,3 +1,50 @@
 ###############################
 # Backend Statefile Resources #
 ###############################
+
+module "s3_statefile" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "fazli-terraform-statefile"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  tags {
+      "Creator" = "Fazli"
+  }
+
+}
+
+module "dynamodb_tfstate_lock" {
+    source = "terraform-aws-modules/dynamodb-table/aws"
+
+    name = "fazli-terraform-statefile-lock"
+    hash_key = "id"
+    range_key = "title"
+
+    attributes = [
+        {
+            name = "id"
+            type = "N"
+        },
+        {
+            name = "title"
+            type = "S"
+        },
+        {
+            name = "age"
+            type = "N"
+        }
+    ]
+
+    tags = {
+        "Creator" = "Fazli"
+    }
+}
+
+############################
+# Infrastructure Resources #
+############################
