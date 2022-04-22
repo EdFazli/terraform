fa419a37cce632d3ffcaadf10c7422d124407cdc
Syed Fazli <edfazli92@gmail.com>
2022-04-22 11:45:54 +0800

    adding backend details

---
+++ backend.tf
@@ -0,0 +1,12 @@
+####################
+# define terraform #
+####################
+terraform {
+    required_version = ">=1.1"
+    backend "s3" {
+        bucket = "fazli-terraform-statefile"
+        key    = "tf/terraform.tfstate"
+        region = "ap-southeast-1"
+        encrypt = true
+  }
+}
