0d4025964663ae8d7190bcea51082bdc8f7593e7
Syed Fazli <edfazli92@gmail.com>
2022-04-22 11:35:41 +0800

    adding variables details

---
+++ variables.tf
@@ -0,0 +1,8 @@
+########################
+# Variable declaration #
+########################
+variable "region" {
+  type = string
+  description = "Which region you want to provision your resources? Default: ap-southeast-1"
+  default = "ap-southeast-1"
+}
