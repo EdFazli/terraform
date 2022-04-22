9f60bf84c96e716e08147314e8284c71eacf5f75
Syed Fazli <edfazli92@gmail.com>
2022-04-23 02:13:25 +0800

    adding redshift subnets

--- main.tf
+++ main.tf
@@ -83,9 +83,10 @@ module "prod_vpc" {
   name = "PROD-VPC"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs               = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  private_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets    = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  redshift_subnets  = ["10.10.41.0/24", "10.10.42.0/24", "10.10.43.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
