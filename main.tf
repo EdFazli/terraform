776c9733b962022f94b36d896fb18b28f0bd6d61
Syed Fazli <edfazli92@gmail.com>
2022-04-23 01:21:05 +0800

    updating main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-##########################################################################
-# Backend Statefile Resources                                            #
-##########################################################################
+########################################################################
+# Backend Statefile Resources                                          #
+########################################################################
 
 module "s3_statefile" {
   source = "terraform-aws-modules/s3-bucket/aws"
@@ -16,7 +16,8 @@ module "s3_statefile" {
   }
 
   tags = {
-      Creator = "Fazli"
+      Creator   = "Fazli"
+      Terraform = "True"
   }
 
 }
@@ -37,10 +38,39 @@ module "dynamodb_tfstate_lock" {
     ]
 
     tags = {
-        Creator = "Fazli"
+        Creator   = "Fazli"
+        Terraform = "True"
     }
 }
 
+########################################################################
+# Data Sources                                                         #
+########################################################################
+
+data "aws_ami" "amazon_windows" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name = "manifest-location"
+    values = ["amazon/Windows_Server-2019-English-Full-Base*"]
+  }
+  filter {
+    name = "platform"
+    values = ["windows"]
+  }
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+  tags = {
+    Creator = "Fazli"  
+    Terraform = "True"
+  }
+
+}
 ########################################################################
 # Infrastructure Resources                                             #
 ########################################################################
@@ -50,7 +80,23 @@ module "dynamodb_tfstate_lock" {
 #-------------------------------VPC------------------------------------#
 
 #-------------------------------EC2------------------------------------#
+module "instance1" {
+  source  = "./modules/ec2"
 
+  name = "Instance-1"
+
+  ami                    = data.aws_ami.amazon_windows.id
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = false
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Creator   = "Fazli"
+    Terraform = "True"
+  }
+}
 #-------------------------------EC2------------------------------------#
 
 #--------------------------------S3------------------------------------#
