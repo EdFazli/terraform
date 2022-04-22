b6e8968dce397548deb1520faa05115a98f0e480
Syed Fazli <edfazli92@gmail.com>
2022-04-22 16:33:15 +0800

    adding data backend credentials info

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-# data "aws_ssm_parameter" "aws_credentials" {
-#   name = "/terraform/aws/credentials"
-# }
+ data "aws_ssm_parameter" "aws_credentials2" {
+   name = "/terraform/aws/credentials"
+ }
 
 #######################
 # Provider Definition #
 #######################
 provider "aws" {
   region = var.region
-  shared_credentials_file = data.aws_ssm_parameter.aws_credentials.value
+  shared_credentials_file = data.aws_ssm_parameter.aws_credentials2.value
 }
