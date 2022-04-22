e3433b0f3a7d672fc556023fb2c49aa314242037
Syed Fazli <edfazli92@gmail.com>
2022-04-23 01:55:34 +0800

    updating main.tf - adding vpc

--- main.tf
+++ main.tf
@@ -77,7 +77,24 @@ data "aws_ami" "amazon_windows" {
 ########################################################################
 
 #-------------------------------VPC------------------------------------#
+module "prod_vpc" {
+  source = "./modules/vpc"
 
+  name = "PROD-VPC"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Creator   = "Fazli"
+    Terraform = "True"
+  }
+}
 #-------------------------------VPC------------------------------------#
 
 #--------------------------------SG------------------------------------#
