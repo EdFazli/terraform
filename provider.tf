35a1b587ab5d6a8b18d37a2fbcdcb977fbbe78e4
Syed Fazli <edfazli92@gmail.com>
2022-04-22 12:35:16 +0800

    updating format for better readable

--- provider.tf
+++ provider.tf
@@ -2,9 +2,9 @@ data "aws_ssm_parameter" "aws_credentials" {
   name = "/terraform/aws/credentials"
 }
 
-###################
-# Define Provider #
-###################
+#######################
+# Provider Definition #
+#######################
 provider "aws" {
   region = var.region
   shared_credentials_file = data.aws_ssm_parameter.aws_credentials.value
